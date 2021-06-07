8bed4b97544005301601565a3a3cbbecbe05f888
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-07 16:35:34 +0900

    fdaf

--- board/write.php
+++ board/write.php
@@ -9,7 +9,29 @@ if(!$_SESSION['loginValue']['SEMAIL']){
 $multi=$_GET["multi"];
 ?>
 
+<!-- Favicon -->
+<link rel="icon" href="/img/core-img/favicon.ico">
 
+<!-- Core Stylesheet -->
+<link href="/style.css" rel="stylesheet">
+
+<!-- Responsive CSS -->
+<link href="/css/responsive/responsive.css" rel="stylesheet">
+
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+<style>
+.childImg {
+max-width:90%;
+padding-bottom:30px;
+margin-top:30px;
+line-height:170%;
+
+}
+</style>
     <!-- ****** Breadcumb Area Start ****** -->
     <div class="breadcumb-area" style="background-image: url(/img/bg-img/breadcumb.jpg);">
         <div class="container h-100">
@@ -81,7 +103,7 @@ $multi=$_GET["multi"];
         </div>
     </div>
     <!-- ****** Contact Area End ****** -->
-    </div>
+
 
 
     <!-- ****** Footer Menu Area Start ****** -->
@@ -94,6 +116,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/inc/footer.php";
 <script>
     $(document).ready(function () {
     var $summernote = $('#summernote').summernote({
+        //airMode: true,
 		codeviewFilter: false,
 		codeviewIframeFilter: true,
         lang: 'ko-KR',
