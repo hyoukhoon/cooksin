222915ec43bea765e590ff7c93b3be23ccf723e0
hyoukhoon <hyoukhoon@gmail.com>
2021-06-11 09:14:09 +0900

    fdsf

--- board/write.php
+++ board/write.php
@@ -1,5 +1,5 @@
 <?php session_start();
-include $_SERVER["DOCUMENT_ROOT"]."/inc/dbconn.php";
+include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";
 if(!$_SESSION['loginValue']['SEMAIL']){
     location_is('','','로그인하십시오.');
     exit;
@@ -16,21 +16,6 @@ $multi=$_GET["multi"];
 
     <!-- Responsive CSS -->
     <link href="/css/responsive/responsive.css" rel="stylesheet">
-
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-  <style>
- .childImg {
-	max-width:90%;
-    padding-bottom:30px;
-    margin-top:30px;
-    line-height:170%;
-
-}
-</style>
 
   <div class="top_header_area">
         <div class="container">
