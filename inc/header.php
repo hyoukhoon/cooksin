e941188f25eb94a84d93e7fb6aa63d9e58dc83d2
hyoukhoon <hyoukhoon@gmail.com>
2021-06-11 09:16:51 +0900

    fasf

--- inc/header.php
+++ inc/header.php
@@ -21,21 +21,6 @@ include $_SERVER["DOCUMENT_ROOT"]."/inc/dbconn.php";
     <!-- Responsive CSS -->
     <link href="/css/responsive/responsive.css" rel="stylesheet">
 
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-  <style>
- .childImg {
-	max-width:90%;
-    padding-bottom:30px;
-    margin-top:30px;
-    line-height:170%;
-
-}
-</style>
-
 </head>
 
 <body>
