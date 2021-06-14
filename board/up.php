51f5e321246f32d7cfa4bdf316f98b7415c34eb5
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-14 17:50:06 +0900

    sfdsa

--- board/up.php
+++ board/up.php
@@ -10,16 +10,40 @@
 </head>
 <body>
 
+<section class="archive-area section_padding_80">
+        <div class="container">
+            <div class="row">
 
+                        <div class="col-12 col-md-12 item">
+                        <div class="contact-form wow fadeInUpBig" data-wow-delay="0.6s">
+                            <form action="#" method="post">
+                            <input type="hidden" name="imgUrl" id="imgUrl" value="">
+                            <input type="hidden" name="attachFile" id="attachFile" value="">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="subject" placeholder="제목">
+                                </div>
+                                <div class="form-group">
                                     <div id="summernote"></div>
+                                </div>
+                                <div class="form-group">
+                                    <div id="attach_site">
+                                        <div id="attachFiles">
+                                        </div>
+                                        <input type="file" multiple class="form-input" name="afile" id="afile" />
+                                    </div>
+                                </div>
                                 <button type="button" class="btn contact-btn"  onclick="saveUp();">WRITE</button>
+                            </form>
+                        </div>
+                    </div>
 
+            </div>
+        </div>
+    </section>
 
   <script>
     $(document).ready(function() {
-        $('.summernote').summernote({
-          //airMode: true
-        });
+        $('.summernote').summernote();
     });
   </script>
 </body>
