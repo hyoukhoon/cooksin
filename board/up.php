5b6d9f313a2c1b3518853e3ed5c818e58b529f27
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-14 17:46:59 +0900

    ㅇㄹㄹㅇ

--- board/up.php
+++ board/up.php
@@ -14,23 +14,13 @@
         <div class="container">
             <div class="row">
 
-                        <div class="col-12 col-md-12 item">
+                    <div class="col-12 col-md-12 item">
                         <div class="contact-form wow fadeInUpBig" data-wow-delay="0.6s">
                             <form action="#" method="post">
                             <input type="hidden" name="imgUrl" id="imgUrl" value="">
                             <input type="hidden" name="attachFile" id="attachFile" value="">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" id="subject" placeholder="제목">
-                                </div>
-                                <div class="form-group">
                                     <div id="summernote"></div>
-                                </div>
-                                <div class="form-group">
-                                    <div id="attach_site">
-                                        <div id="attachFiles">
-                                        </div>
-                                        <input type="file" multiple class="form-input" name="afile" id="afile" />
-                                    </div>
                                 </div>
                                 <button type="button" class="btn contact-btn"  onclick="saveUp();">WRITE</button>
                             </form>
