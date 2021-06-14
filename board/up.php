32c44d37b3876550af1271c793d677239b3932dd
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-14 17:53:20 +0900

    dafd

--- board/up.php
+++ board/up.php
@@ -9,7 +9,47 @@
   <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 </head>
 <body>
+<section class="archive-area section_padding_80">
+        <div class="container">
+            <div class="row">
+
+                        <div class="col-12 col-md-12 item">
+                        <div class="contact-form wow fadeInUpBig" data-wow-delay="0.6s">
+                            <h2 class="contact-form-title mb-30"><?php echo multi_is($multi);?> 글쓰기</h2>
+                            <!-- Contact Form -->
+                            <form action="#" method="post">
+                            <input type="hidden" name="multi" id="multi" value="<?php echo $multi;?>">
+                            <input type="hidden" name="imgUrl" id="imgUrl" value="">
+                            <input type="hidden" name="attachFile" id="attachFile" value="">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="subject" placeholder="제목">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="childName" placeholder="태그">
+                                </div>
+                                <div class="form-group">
   <div id="summernote"><p>Hello Summernote</p></div>
+                                </div>
+                                <div class="form-group">
+                                    <div id="attach_site">
+                                        <div id="attachFiles">
+                                        </div>
+                                        <input type="file" multiple class="form-input" name="afile" id="afile" />
+                                    </div>
+                                </div>
+                                <button type="button" class="btn contact-btn"  onclick="saveUp();">WRITE</button>
+                            </form>
+                        </div>
+                    </div>
+
+            
+
+                
+                
+
+            </div>
+        </div>
+    </section>  
 </body>
 
 <script>
