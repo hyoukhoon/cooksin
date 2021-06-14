d28ca25541f4f4df93646e07f448fcd0133541ab
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-14 17:51:56 +0900

    fdsaf

--- board/up.php
+++ board/up.php
@@ -9,42 +9,11 @@
   <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 </head>
 <body>
+  <div id="summernote"><p>Hello Summernote</p></div>
+</body>
 
-<section class="archive-area section_padding_80">
-        <div class="container">
-            <div class="row">
-
-                        <div class="col-12 col-md-12 item">
-                        <div class="contact-form wow fadeInUpBig" data-wow-delay="0.6s">
-                            <form action="#" method="post">
-                            <input type="hidden" name="imgUrl" id="imgUrl" value="">
-                            <input type="hidden" name="attachFile" id="attachFile" value="">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" id="subject" placeholder="제목">
-                                </div>
-                                <div class="form-group">
-                                    <div id="summernote"></div>
-                                </div>
-                                <div class="form-group">
-                                    <div id="attach_site">
-                                        <div id="attachFiles">
-                                        </div>
-                                        <input type="file" multiple class="form-input" name="afile" id="afile" />
-                                    </div>
-                                </div>
-                                <button type="button" class="btn contact-btn"  onclick="saveUp();">WRITE</button>
-                            </form>
-                        </div>
-                    </div>
-
-            </div>
-        </div>
-    </section>
-
-  <script>
+<script>
     $(document).ready(function() {
-        $('.summernote').summernote();
+        $('#summernote').summernote();
     });
   </script>
-</body>
-</html>
