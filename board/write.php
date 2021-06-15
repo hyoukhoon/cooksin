58a5bbb87fd10fb92016351b50234b2495577b5a
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-15 17:37:31 +0900

    fdaf

--- board/write.php
+++ board/write.php
@@ -1,4 +1,4 @@
-<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";?>
+<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/dbconn.php";?>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -11,7 +11,7 @@
   <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 </head>
-
+<body>
 <section class="archive-area section_padding_80">
         <div class="container">
             <div class="row">
@@ -53,8 +53,7 @@
             </div>
         </div>
     </section>  
-<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";?>    
-
+</body>
 <script>
     $(document).ready(function () {
     var $summernote = $('#summernote').summernote({
