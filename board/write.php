8bcc13c0ee69429508209d04601453340756f61d
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-15 14:21:05 +0900

    fdsaf

--- board/write.php
+++ board/write.php
@@ -1,59 +1,26 @@
-<?php
-// include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";
-// if(!$_SESSION['loginValue']['SEMAIL']){
-//     location_is('','','로그인하십시오.');
-//     exit;
-// }
-
-$multi=$_GET["multi"];
-?>
-
-<!-- include summernote css/js -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, 
+  maximum-scale=1.0, minimum-scale=1.0">
+  <title>Summernote</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-
-    <!-- ****** Breadcumb Area Start ****** -->
-    <div class="breadcumb-area" style="background-image: url(/img/bg-img/breadcumb.jpg);">
-        <div class="container h-100">
-            <div class="row h-100 align-items-center">
-                <div class="col-12">
-                    <div class="bradcumb-title text-center">
-                        <h2><?php //echo multi_is($multi);?></h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="breadcumb-nav">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page"><?php //echo multi_is($multi);?></li>
-                        </ol>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- ****** Breadcumb Area End ****** -->
-
-    <!-- ****** child Area Start ****** -->
-    <section class="archive-area section_padding_80">
+</head>
+<body>
+<section class="archive-area section_padding_80">
         <div class="container">
             <div class="row">
 
                         <div class="col-12 col-md-12 item">
                         <div class="contact-form wow fadeInUpBig" data-wow-delay="0.6s">
-                            <h2 class="contact-form-title mb-30"><?php //echo multi_is($multi);?> 글쓰기</h2>
+                            
                             <!-- Contact Form -->
                             <form action="#" method="post">
-                            <input type="hidden" name="multi" id="multi" value="<?php echo $multi;?>">
+                            
                             <input type="hidden" name="imgUrl" id="imgUrl" value="">
                             <input type="hidden" name="attachFile" id="attachFile" value="">
                                 <div class="form-group">
@@ -63,7 +30,7 @@ $multi=$_GET["multi"];
                                     <input type="text" class="form-control" id="childName" placeholder="태그">
                                 </div>
                                 <div class="form-group">
-                                    <div id="summernote">test</div>
+                                <div id="summernote"></div>
                                 </div>
                                 <div class="form-group">
                                     <div id="attach_site">
@@ -84,16 +51,28 @@ $multi=$_GET["multi"];
 
             </div>
         </div>
-    </section>
-    <!-- ****** child Area End ****** -->
+    </section>  
+</body>
 
- 
-    <!-- ****** Our Creative Portfolio Area End ****** -->
 <script>
-$(document).ready(function() {
-  $('#summernote').summernote();
-});
-</script>
-<?php
-include $_SERVER["DOCUMENT_ROOT"]."/inc/footer.php";
-?>
+     $(document).ready(function() {
+      $('#summernote').summernote({
+        placeholder: '글을 입력해 주세요',
+        height: 500,
+        lang: 'ko-KR',
+        toolbar: [
+                    // [groupName, [list of button]]
+                    ['Font Style', ['fontname']],
+                    ['style', ['bold', 'italic', 'underline']],
+                    ['font', ['strikethrough']],
+                    ['fontsize', ['fontsize']],
+                    ['color', ['color']],
+                    ['para', ['paragraph']],
+                    ['height', ['height']],
+                    ['Insert', ['picture']],
+                    ['Insert', ['link']],
+                    ['Misc', ['fullscreen']]
+                 ]
+      });
+    });
+  </script>
