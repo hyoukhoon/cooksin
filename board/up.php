d52d4c4923866c15786bfef4a6d1209212e3c8f4
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-15 14:12:48 +0900

    fdsaf

--- board/up.php
+++ board/up.php
@@ -1,6 +1,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, 
+  maximum-scale=1.0, minimum-scale=1.0">
   <title>Summernote</title>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
