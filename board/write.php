bb7ee862e7b1049fc94f8db813bc2cec07a73879
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-15 14:32:53 +0900

    fdaf

--- board/write.php
+++ board/write.php
@@ -1,4 +1,4 @@
-<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";?>
+<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/dbconn.php";?>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -6,12 +6,12 @@
   maximum-scale=1.0, minimum-scale=1.0">
   <title>Summernote</title>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-  
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 </head>
-
+<body>
 <section class="archive-area section_padding_80">
         <div class="container">
             <div class="row">
@@ -53,8 +53,7 @@
             </div>
         </div>
     </section>  
-
-<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/footer.php";?>
+</body>
 
 <script>
      $(document).ready(function() {
