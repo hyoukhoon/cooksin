a153a124a5217bb76a9a8b23858c6db3a57e0349
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-15 14:26:37 +0900

    fdafd

--- board/write.php
+++ board/write.php
@@ -7,6 +7,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/inc/dbconn.php";
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, 
   maximum-scale=1.0, minimum-scale=1.0">
   <title>Summernote</title>
+  <link href="/css/responsive/responsive.css" rel="stylesheet">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
