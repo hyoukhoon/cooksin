77c5303f32742aed90881e945bca179d7fc15883
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-15 14:32:02 +0900

    dsaf

--- inc/footer.php
+++ inc/footer.php
@@ -88,7 +88,7 @@
     <!-- ****** Footer Menu Area End ****** -->
 
     <!-- Jquery-2.2.4 js -->
-    
+    <script src="/js/jquery/jquery-2.2.4.min.js"></script>
     <!-- Popper js -->
     <script src="/js/bootstrap/popper.min.js"></script>
     <!-- Bootstrap-4 js -->
