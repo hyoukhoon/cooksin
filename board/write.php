21367014970769e7be49eb1ebb1a5e05b840bca9
hyoukhoon <hyoukhoon@cnspartner.com>
2021-06-15 14:25:42 +0900

    fda

--- board/write.php
+++ board/write.php
@@ -1,4 +1,6 @@
-<?php include $_SERVER["DOCUMENT_ROOT"]."/inc/dbconn.php";?>
+<?php session_start();
+include $_SERVER["DOCUMENT_ROOT"]."/inc/dbconn.php";
+?>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -12,6 +14,126 @@
   <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 </head>
 <body>
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="yummy-load"></div>
+    </div>
+
+    <!-- Background Pattern Swither -->
+    <!-- <div id="pattern-switcher">
+        Bg Pattern
+    </div>
+    <div id="patter-close">
+        <i class="fa fa-times" aria-hidden="true"></i>
+    </div> -->
+
+    <!-- ****** Top Header Area Start ****** -->
+    <div class="top_header_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-5 col-sm-6">
+                    <!--  Top Social bar start -->
+                    <div class="top_social_bar">
+                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <!--  Login Register Area -->
+                <div class="col-7 col-sm-6">
+                    <div class="signup-search-area d-flex align-items-center justify-content-end">
+                        <div class="login_register_area d-flex">
+                            <div class="login">
+                                <?php if($_SESSION['loginValue']['SEMAIL']){?>
+                                    <a href="/member/mypage.php">Mypage</a> 
+                                <?php }else{?>
+                                    <a href="/member/login.php">Login</a>
+                                <?php }?>
+                            </div>
+                            <div class="register">
+                                <?php if($_SESSION['loginValue']['SEMAIL']){?>
+                                    <a href="/member/logout.php">Logout</a>
+                                <?php }else{?>
+                                    <a href="/member/signup.php">Register</a>
+                                <?php }?>
+                            </div>
+                        </div>
+                        <!-- Search Button Area -->
+                        <!-- <div class="search_button">
+                            <a class="searchBtn" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                        </div>
+                        
+                        <div class="search-hidden-form">
+                            <form action="#" method="get">
+                                <input type="search" name="search" id="search-anything" placeholder="Search Anything...">
+                                <input type="submit" value="" class="d-none">
+                                <span class="searchBtn"><i class="fa fa-times" aria-hidden="true"></i></span>
+                            </form>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ****** Top Header Area End ****** -->
+    <!-- ****** Header Area Start ****** -->
+    <header class="header_area">
+        <div class="container">
+            <div class="row">
+                <!-- Logo Area Start -->
+                <div class="col-12">
+                    <div class="logo_area text-center">
+                        <a href="/" class="yummy-logo">Cooksin Yummy Site</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#yummyfood-nav" aria-controls="yummyfood-nav" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars" aria-hidden="true"></i> Menu</button>
+                        <!-- Menu Area Start -->
+                        <div class="collapse navbar-collapse justify-content-center" id="yummyfood-nav">
+                            <ul class="navbar-nav" id="yummy-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <!-- <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="yummyDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+                                    <div class="dropdown-menu" aria-labelledby="yummyDropdown">
+                                        <a class="dropdown-item" href="/">Home</a>
+                                        <a class="dropdown-item" href="/board/list.php">게시판</a>
+                                        <a class="dropdown-item" href="/single.html">Single Blog</a>
+                                        <a class="dropdown-item" href="/static.html">Static Page</a>
+                                        <a class="dropdown-item" href="/contact.html">Contact</a>
+                                    </div>
+                                </li> -->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/board/list.php?multi=recipes">RECIPES</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/board/list.php?multi=food">FOOD</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/board/list.php?multi=restaurant">RESTAURANT</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/board/list.php?multi=free">FREE</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ****** Header Area End ****** -->
+
 <section class="archive-area section_padding_80">
         <div class="container">
             <div class="row">
